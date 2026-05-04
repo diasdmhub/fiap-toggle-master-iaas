922afc129d35d7f6b3eacd65afa2c8165fb03734
diasdm <diasdm@hotmail.com>
2026-05-03 21:28:41 -0300

    Adicionados os módulos secret

--- argo/argo_update.sh
+++ argo/argo_update.sh
@@ -20,10 +20,11 @@ fi
 
 cp argo/argo_deploy.yaml.example argo/argo_deploy.yaml
 
-sed -i -E "s|^(\s+)repoURL: .*|\1repoURL: $(git config --get remote.origin.url)|" argo/argo_deploy.yaml
+sed -i -E "s|^(\s+)repoURL: .*|\1repoURL: $(git config --get remote.origin.url)|" argo/argo_deploy.yaml && \
+    echo "URL do repositório atualizada"
 
 sed -i "/serviceAccountRef/,/value:/ s|^\(\s*\)value:\s*$|\1value: $(terraform output -json es_outputs | jq -r .external_secrets_role_name)|" argo/argo_deploy.yaml && \
-    echo "Nome do External Secrets service account atualizada"
+    echo "Nome do External Secrets service account atualizado"
 
 for serv in "auth" "flag" "targeting" "evaluation" "analytics"; do
     SERVICE=$(terraform output -json ecr_outputs | jq -r .ecr_repository_urls.$serv)
